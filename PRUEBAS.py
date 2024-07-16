864ee76d05f75f37808cd1cc7cecb52739e9c372
pMorales98 <96068292+pMorales98@users.noreply.github.com>
2024-07-15 18:47:17 -0600

    first commit

---
+++ PRUEBAS.py
@@ -0,0 +1,19 @@
+
+
+# Define a function named score
+def score():
+    global score 
+    score = 0
+    print(score)
+
+# Call the function score
+
+score()
+
+def promedio(numeros):
+total = sum(numeros)
+    promedio = total / len(numero)
+    print(promedio)
+
+
+promedio(numeros=[1,2,3])
